69854d397b6c2b5c7e70c0c3d71d7f6129d1e6ca
talarkn <talar.kijilian19@gmail.com>
2022-12-16 09:33:06 +0100

    In getComputerChoice() function change the choice variable's name to computerChoice

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function getComputerChoice() {
-    let choice = Math.floor(Math.random()*3);
-    if (choice === 0) {
+    let computerChoice = Math.floor(Math.random()*3);
+    if (computerChoice === 0) {
         return 'Paper';
     } else if (choice === 1) {
         return 'Rock';
