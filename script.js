649b8ffadb4831a48a617e0a7a569bbf32d302a3
talarkn <talar.kijilian19@gmail.com>
2022-12-18 15:34:52 +0100

    Develop getPlayerChoice function, make correction
    Correct the choice variable in getComputerChoice() function. Change variable
    in line 5 to computerChoice.
    Develop getPlayerChoice() function. If user cancels the prompt, alert
    with 'Game is over!'. If user inputs an invalid choice, alert it. If user
    input either 'Rock', 'Paper' or 'Scissors', case insensitive,
    convert the input to uppercase and compare it with 'ROCK', 'PAPER',
    'SCISSORS' to make sure the input is valid, then return the input.

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function getComputerChoice() {
     let computerChoice = Math.floor(Math.random()*3);
     if (computerChoice === 0) {
         return 'Paper';
-    } else if (choice === 1) {
+    } else if (computerChoice === 1) {
         return 'Rock';
     } else {
         return 'Scissors';
@@ -11,7 +11,13 @@ function getComputerChoice() {
 
 function getPlayerChoice() {
     let playerChoice = prompt('Let\'s play a game: Rock, Paper or Scissors?');
-    return playerChoice;
+    if (!playerChoice) {
+        alert('Game is cancelled.');
+    } else if (playerChoice.toUpperCase() === 'ROCK' || playerChoice.toUpperCase() === 'PAPER' || playerChoice.toUpperCase() === 'SCISSORS') {
+        return playerChoice;
+    } else {
+        alert('Invalid input, try again!');
+    }
 }
 
 let computerSelection = getComputerChoice();
