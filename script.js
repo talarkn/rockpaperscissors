f2495b59d279cc8797a37361c6449daaf4013dd1
talarkn <talar.kijilian19@gmail.com>
2023-02-05 17:12:00 +0100

    Change and add condition spot within playRound()
    Change condition, which checks whether playerScore or computerScore
    is greater than 4, to the last part of the playRound() function.
    Add condition in the first part of playRound(): only execute the function
    if the text content of the final div is an empty string. The game will
    end once a winner is announced.
    Move around some variables.

--- script.js
+++ script.js
@@ -5,21 +5,33 @@ const buttons = document.querySelectorAll('.rps');
 const result = document.querySelector('.result');
 const score = document.querySelector('.score');
 const final = document.querySelector('.final');
+const restartBtn = document.querySelector('.restart');
+
+buttons.forEach(button => button.addEventListener('click', () => playRound(button.value, getComputerChoice())));
+restartBtn.addEventListener('click', restart); 
 
 function getComputerChoice() {
     let computerChoice = Math.floor(Math.random()*3);
-    if (computerChoice === 0) {
-        return 'Paper';
-    } else if (computerChoice === 1) {
-        return 'Rock';
-    } else {
-        return 'Scissors';
-    }
+    if (computerChoice === 0) return 'Paper';
+    else if (computerChoice === 1) return 'Rock';
+    else return 'Scissors';
 }
 
-buttons.forEach(button => button.addEventListener('click', () => playRound(button.value, getComputerChoice())));
-
-function playRound(playerSelection, computerSelection) {
+function playRound(playerSelection, computerSelection) { // the buttons need to be clicked to get the final result
+    if (final.textContent === '') {
+        if (playerSelection === computerSelection) {
+            result.textContent = 'It\'s a draw!';
+        } else if ((playerSelection === 'Rock' && computerSelection === 'Scissors') ||
+    (playerSelection === 'Paper' && computerSelection === 'Rock') || 
+    (playerSelection === 'Scissors' && computerSelection === 'Paper')) {
+            playerScore++;
+            result.textContent = `You win! ${playerSelection} beats ${computerSelection}.`;
+        } else {
+            computerScore++;
+            result.textContent = `You lose! ${computerSelection} beats ${playerSelection}.`;
+        }   
+        score.textContent = `You: ${playerScore} vs Computer: ${computerScore}`;
+    }
     if (playerScore > 4) {
         final.textContent = 'You win! The odds were in your favour!';
         return;
@@ -27,25 +39,8 @@ function playRound(playerSelection, computerSelection) {
         final.textContent = 'The computer wins! Better luck next time.';
         return;
     }
-
-    if (playerSelection === ' ') {
-        return null;
-    } else if (playerSelection === computerSelection) {
-        result.textContent = 'It\'s a draw!';
-    } else if ((playerSelection === 'Rock' && computerSelection === 'Scissors') ||
-(playerSelection === 'Paper' && computerSelection === 'Rock') || 
-(playerSelection === 'Scissors' && computerSelection === 'Paper')) {
-        playerScore++;
-        result.textContent = `You win! ${playerSelection} beats ${computerSelection}.`;
-    } else {
-        computerScore++;
-        result.textContent = `You lose! ${computerSelection} beats ${playerSelection}.`;
-    }   
-    score.textContent = `You: ${playerScore} vs Computer: ${computerScore}`;
 }
 
-const restartBtn = document.querySelector('.restart');
-restartBtn.addEventListener('click', restart); 
 
 function restart() {
     playerScore = 0;
